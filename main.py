7039b3c2c9caf8550352a311a9151c9d29baeb3d
ben <30186471+Gammage@users.noreply.github.com>
2026-05-28 23:34:16 +0100

    changed from uv to nix, started project

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 import pandas as pd
 import numpy
 import matplotlib.pyplot as plt
-
+import sqlite3
 
 def main():
-    print("Hello from us-consumer-finance-complaints!")
 
+    conn = sqlite3.connect("./data/database.sqlite")
 
+#list all tables (see what exists)
+    tables = pd.read_sql("SELECT name from sqlite_master WHERE type='table'", conn)
+    print(tables)
+
+# pick a table name from step 1, then inspect columns
+    schema = pd.read_sql("PRAGMA table_info(consumer_complaints)", conn)
+    print(schema)
+
+    conn.close()
 
 if __name__ == "__main__":
     main()
---
+++ shell.nix
@@ -0,0 +1,15 @@
+let
+  pkgs = (builtins.getFlake "/home/ben/nixos").inputs.nixpkgs.legacyPackages.${builtins.currentSystem};
+in
+pkgs.mkShell {
+  packages = with pkgs; [
+    (python3.withPackages (ps: with ps; [
+      jupyterlab
+      jupytext
+      ipykernel
+      numpy
+      pandas
+      matplotlib
+    ]))
+  ];
+}
